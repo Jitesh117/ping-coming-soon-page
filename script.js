20c6d80aa4c319086e486177e9b052b241d779af
Jitesh117 <sahookumarjitesh@gmail.com>
2022-02-27 16:20:02 +0530

    changed some js

--- script.js
+++ script.js
@@ -2,23 +2,17 @@ function ValidateEmail()
 {
 let none = document.getElementById('none');
 let email = document.getElementById('email')
-// let nonetext = document.getElementById('nonetext');
 var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 if(email.value.match(mailformat))
 {
-// document.form1.text1.focus();
-none.style.display = 'none';
-email.style.borderColor = 'var(--blue)';
-// nonetext.style.display = 'none';
-
-// return true;
+    none.style.display = 'none';
+    email.style.borderColor = 'var(--blue)';
 }
 else
 {
     none.style.display= 'initial';
     email.style.borderColor = 'red';
-// nonetext.style.display= 'inherit';
-return false;
+    // none.style.marginBottom = '5px';
 }
 
 }
